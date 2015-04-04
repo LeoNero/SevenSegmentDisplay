694fef9d2eb80c8387b70b5bd2ce6c28af10abe0
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2015-04-03 22:59:06 -0300

    Update ProjetoDisplay7Pot.ino

--- ProjetoDisplay7Pot.ino
+++ ProjetoDisplay7Pot.ino
@@ -10,6 +10,7 @@ void loop()
 {
 	displayLed.numberZero(); //Exibirá o numero 0 no display de 7 segmentos
 	displayLed.numberOne(500); //Você tambem pode escolher o delay da exibicao. Padrao é 1000
+	//Vai de numberZero até numberNine :D
 
 	displayLed.chooseNumbers(4, 8); //Você pode escolher os numeros que quer exibir em um intervalor. No caso, de 4 a 8
 	displayLed.chooseNumbers(4, 8, 300); //Voce tambem pode escolher o tempo do delay. Padrao é 1000 
