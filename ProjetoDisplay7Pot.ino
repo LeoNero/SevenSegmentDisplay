e1318e9b4f1ae02c993162ec82476fdfaaee0276
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2015-06-11 20:26:30 -0300

    Update ProjetoDisplay7Pot.ino

--- ProjetoDisplay7Pot.ino
+++ ProjetoDisplay7Pot.ino
@@ -1,6 +1,6 @@
 #include "MiniBiblioteca.h"
 
-DisplaySeven displayLed(3, 4, 5, 6); //As quatro portas conectadas ao Circuito Integrado
+DisplaySeven displayLed(3, 4, 5, 6); 
 
 void setup() 
 {
@@ -8,13 +8,12 @@ void setup()
 
 void loop() 
 {
-	displayLed.numberZero(); //Exibirá o numero 0 no display de 7 segmentos
-	displayLed.numberOne(500); //Você tambem pode escolher o delay da exibicao. Padrao é 1000
-	//Vai de numberZero até numberNine :D
+	displayLed.numberZero(); 
+	displayLed.numberOne(500); 
 
-	displayLed.chooseNumbers(4, 8); //Você pode escolher os numeros que quer exibir em um intervalor. No caso, de 4 a 8
-	displayLed.chooseNumbers(4, 8, 300); //Voce tambem pode escolher o tempo do delay. Padrao é 1000 
+	displayLed.chooseNumbers(4, 8); 
+	displayLed.chooseNumbers(4, 8, 300); 
 
-	displayLed.allNumbers(); //Exibirá todos os números (de 0 a 9)
-	displayLed.allNumbers(500); //Você também pode escolher o tempo do delay. Padrao é 1000
+	displayLed.allNumbers(); 
+	displayLed.allNumbers(500); 
 }
